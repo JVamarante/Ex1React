127c5ae52e0489a08c1c67414f1d3d061813f965
JVamarante <joaovamarante@gmail.com>
2025-02-26 19:41:01 -0300

    commit

---
+++ aula1/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Adicao from './components/Adicao'
+
+const App = () => {
+  return (
+    <div>
+      <Adicao num1={5} num2={8} />
+    </div>
+  )
+}
+
+export default App
+
+import React from 'react'
+import Subtracao from './components/Subtracao'
+
+const App = () => {
+  return (
+    <div>
+      <Subtracao num1={2} num2={5} />
+    </div>
+  )
+}
+
+export default App
