8bd0eaaae057e784dee730f773e944b3f481d12f
JVamarante <joaovamarante@gmail.com>
2025-04-25 11:58:51 -0300

    aula5

---
+++ aula5/src/components/ListadeAlunos.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+
+const ListadeAlunos = () => {
+
+    const [alunos, setAlunos] = useState([])
+    
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch("http://localhost:3000/alunos")
+            const data = await response.json()
+            setAlunos(data)
+        }
+        fetchData()
+    }, [])
+    
+  return (
+    <div>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Matricula:</th>
+                    <th>Nome:</th>
+                    <th>Email:</th>
+                </tr>
+            </thead>
+            <tbody>
+                {alunos.map((aluno) => (
+                    <tr key={aluno.id}>
+                        <td>{aluno.id}</td>
+                        <td>{aluno.nomel}</td>
+                        <td>{aluno.email}</td>
+                    </tr>
+                ))
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+
+export default ListadeAlunos
